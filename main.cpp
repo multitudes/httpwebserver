40628135a1cf337f4ea0b7ddeee5ed704ee2ed37
Laurent Brusa <wrmultitudes@gmail.com>
2025-03-14 15:57:55 +0100

    example of upload

--- main.cpp
+++ main.cpp
@@ -27,19 +27,19 @@
  * "Content-Type: application/json" --data-raw '{"message": "This is a test"}'
  * localhost:4244 curl -I -H "Content-Type: application/json"
  * http://localhost:4244
+ * 
+ * this will create multipart file upload
+ * curl -F "file=@test.txt" http://localhost:4244/test.txt
+ * 
+ * to test the file upload
+ * curl --data-binary @test.txt http://localhost:4244/test.txt
  */
 // Connection structure to store client information and pipe file descriptors
 typedef struct connection_s {
   int client_fd;
   int file_fd;
-  long file_size; // off_t? is
-  long file_offset;
-  bool headers_sent;
-  int child_stdin_pipe[2];  // [0] for read, [1] for write
-  int child_stdout_pipe[2]; // [0] for read, [1] for write
-  pid_t child_pid;
-  int is_sending;
-  int is_receiving;
+  char filename[256]; // Store filename
+  bool is_uploading;
 } connection_t;
 
 // Global variables to track connections and poll fds
@@ -56,122 +56,11 @@ void add_to_poll(int fd, short events) {
   poll_fd_count++;
 }
 
-/**
- * for client to server communication
- */
-
-// Prepare to serve the index.html file
-int prepare_file_response(int idx) {
-  connection_t *conn = &connections[idx];
-  struct stat file_stat;
-
-  conn->file_fd = open("index.html", O_RDONLY);
-  if (conn->file_fd < 0) {
-    perror("Failed to open index.html");
-    return -1;
-  }
-
-  if (fstat(conn->file_fd, &file_stat) < 0) {
-    perror("Failed to get file stats");
-    close(conn->file_fd);
-    conn->file_fd = -1;
-    return -1;
-  }
-
-  conn->file_size = file_stat.st_size;
-  conn->file_offset = 0;
-  conn->headers_sent = 0;
-  add_to_poll(conn->client_fd, POLLOUT); // Poll client FD for writing
-  add_to_poll(conn->file_fd, POLLIN);    // Poll file FD for reading
-  return 0;
-}
-
-// Send HTTP response headers
-int send_response_headers(int conn_idx) {
-  connection_t *conn = &connections[conn_idx];
-  char headers[512];
-
-  // Create HTTP response headers
-  snprintf(headers, sizeof(headers),
-           "HTTP/1.1 200 OK\r\n"
-           "Content-Type: text/html\r\n"
-           "Content-Length: %ld\r\n"
-           "Connection: close\r\n"
-           "\r\n",
-           conn->file_size);
-
-  // Send headers
-  int bytes_sent = send(conn->client_fd, headers, strlen(headers), 0);
-  if (bytes_sent < 0) {
-    perror("Failed to send headers");
-    return -1;
-  }
-
-  conn->headers_sent = 1;
-  return bytes_sent;
-}
-
-// Send a chunk of the file to the client
-int send_file_chunk(int conn_idx) {
-  connection_t *conn = &connections[conn_idx];
-  char buffer[BUFFER_SIZE];
-
-  // Calculate bytes to read (up to buffer size)
-  size_t bytes_to_read = BUFFER_SIZE;
-  if (conn->file_offset + bytes_to_read > conn->file_size) {
-    bytes_to_read = conn->file_size - conn->file_offset;
-  }
-
-  // Read from file
-  ssize_t bytes_read =
-      pread(conn->file_fd, buffer, bytes_to_read, conn->file_offset);
-  if (bytes_read <= 0) {
-    if (bytes_read < 0) {
-      perror("Failed to read file");
-    }
-    return -1;
-  }
-
-  // Send to client
-  ssize_t bytes_sent = send(conn->client_fd, buffer, bytes_read, 0);
-  if (bytes_sent < 0) {
-    perror("Failed to send file chunk");
-    return -1;
-  }
-
-  // Update file offset
-  conn->file_offset += bytes_sent;
-
-  printf("Sent %ld bytes of file data (%ld/%ld)\n", bytes_sent,
-         conn->file_offset, conn->file_size);
-
-  // Check if we've sent the entire file
-  if (conn->file_offset >= conn->file_size) {
-    printf("File transfer complete\n");
-    return 0; // Done sending
-  }
-
-  return 1; // More data to send
-}
-
 // Initialize a new connection
 void init_connection(connection_t *conn) {
-  conn->client_fd = -1;
-
-  // for file transfer
-  conn->file_fd = -1;
-  conn->file_offset = 0;
-  conn->file_size = 0;
-  conn->headers_sent = 0;
-
-  // for cgi
-  conn->child_stdin_pipe[0] = -1;
-  conn->child_stdin_pipe[1] = -1;
-  conn->child_stdout_pipe[0] = -1;
-  conn->child_stdout_pipe[1] = -1;
-  conn->child_pid = -1;
-  conn->is_sending = 0;
-  conn->is_receiving = 0;
+	conn->client_fd = -1;
+	conn->file_fd = -1;
+	memset(conn->filename, 0, sizeof(conn->filename));
 }
 
 // Find an available connection slot
@@ -187,9 +76,7 @@ int find_free_connection() {
 // Find the connection index for a given file descriptor
 int find_connection_by_fd(int fd) {
   for (int i = 0; i < MAX_CONNECTIONS; i++) {
-    if (connections[i].client_fd == fd || connections[i].file_fd == fd ||
-        connections[i].child_stdin_pipe[1] == fd ||
-        connections[i].child_stdout_pipe[0] == fd) {
+    if (connections[i].client_fd == fd || connections[i].file_fd == fd) {
       return i;
     }
   }
@@ -200,26 +87,12 @@ int find_connection_by_fd(int fd) {
 void close_connection(int conn_idx) {
   connection_t *conn = &connections[conn_idx];
 
-  if (conn->client_fd != -1) {
-    close(conn->client_fd);
-  }
+  if (conn->client_fd != -1) close(conn->client_fd);
+  if (conn->file_fd != -1) close(conn->file_fd);
+  memset(conn, 0, sizeof(connection_t));
+  conn->client_fd = -1;
+  conn->file_fd = -1;
 
-  if (conn->child_stdin_pipe[0] != -1)
-    close(conn->child_stdin_pipe[0]);
-  if (conn->child_stdin_pipe[1] != -1)
-    close(conn->child_stdin_pipe[1]);
-  if (conn->child_stdout_pipe[0] != -1)
-    close(conn->child_stdout_pipe[0]);
-  if (conn->child_stdout_pipe[1] != -1)
-    close(conn->child_stdout_pipe[1]);
-
-  // Kill child process if it's still running
-  if (conn->child_pid > 0) {
-    kill(conn->child_pid, SIGTERM);
-  }
-
-  // Reset the connection
-  init_connection(conn);
 }
 
 // Update the poll_fds array (rebuild it from scratch)
@@ -326,30 +199,57 @@ int main() {
 
       connection_t *conn = &connections[idx];
 
-      if (fd == conn->client_fd && (poll_fds[i].revents & POLLIN)) {
-        char buffer[BUFFER_SIZE];
-        ssize_t bytes_read = recv(conn->client_fd, buffer, BUFFER_SIZE - 1, 0);
-        if (bytes_read <= 0) {
-          printf("Client disconnected\n");
-          close_connection(idx);
-          continue;
-        }
-        buffer[bytes_read] = '\0';
-        printf("Received %ld bytes: %s\n", bytes_read, buffer);
+	  if (fd == conn->client_fd && (poll_fds[i].revents & POLLIN)) {
+		char buffer[BUFFER_SIZE];
+		ssize_t bytes_read = recv(conn->client_fd, buffer, BUFFER_SIZE, 0);
 
-        if (prepare_file_response(idx) < 0) {
-          close_connection(idx);
-        }
-		update_poll_fds(server_fd); // Update after new file added.
-      } else if (fd == conn->client_fd && (poll_fds[i].revents & POLLOUT)) {
-        if (!conn->headers_sent) {
-          if (send_response_headers(idx) < 0) {
-            close_connection(idx);
-          }
-        } else if (send_file_chunk(idx) < 0) {
-          close_connection(idx);
-        }
-      }
+		if (bytes_read <= 0) {
+			printf("Client disconnected\n");
+			close_connection(idx);
+			update_poll_fds(server_fd);
+			continue;
+		}
+		printf("Received %ld bytes\nreq is\n%s", bytes_read, buffer);
+
+		if (!conn->is_uploading) {
+			// Simple filename extraction
+			strncpy(conn->filename, buffer, sizeof(conn->filename) - 1);
+			conn->filename[sizeof(conn->filename) - 1] = '\0';
+			conn->file_fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (conn->file_fd != -1) {
+				conn->is_uploading = true;
+				update_poll_fds(server_fd);
+				printf("File opened successfully for writing: test.txt\n");
+			} else {
+				perror("Failed to open file");
+				close_connection(idx);
+				update_poll_fds(server_fd);
+			}
+			// this will not happen because the whole req 
+			// is already consumed by the recv call previous to this.
+			// but folr bigger uploads it will get thet data in parts
+			// with curl -F option is sending multipart data...
+			// we can chose not to handle it? 
+			// or it sends raw data in parts? more logic is needed
+		} 
+
+		// here if the file is already opened and we are getting more of the file
+		if (conn->is_uploading) {
+			write(conn->file_fd, buffer, bytes_read);
+			if (bytes_read < BUFFER_SIZE) {
+				printf("Upload complete\n");
+				// close_connection(idx);
+				// update_poll_fds(server_fd);
+			}
+		}
+	} else if (fd == conn->client_fd && (poll_fds[i].revents & POLLOUT)) {
+		
+			char response[] = "HTTP/1.1 200 OK\r\nContent-Length: 18\r\nConnection: close\r\n\r\nUpload successful\n";
+			send(conn->client_fd, response, strlen(response), 0);
+			close_connection(idx);
+			update_poll_fds(server_fd);
+		
+	}
     }
   }
 
