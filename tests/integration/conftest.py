d4c01fc8928998af9bf169bf89e0e2a67971154f
Laurent Brusa <wrmultitudes@gmail.com>
2025-03-24 14:08:01 +0100

    add the tests

---
+++ tests/integration/conftest.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+import pytest
+
+@pytest.fixture(scope="session", autouse=True)
+def start_web_server():
+    # Start the web server
+    # server_process = subprocess.Popen(["./webserv"])
+    server_process = subprocess.Popen(["./a.out"])
+        
+    # Wait for the server to start
+    time.sleep(2)
+    
+    yield server_process
+    
+    # Teardown: Stop the web server
+    server_process.terminate()
+    server_process.wait()
---
+++ tests/tests.md
@@ -0,0 +1,15 @@
+## pytest
+
+To test locally you need to change directory to the `tests` folder and run the following command:
+
+```python
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+then 
+```python
+pytest integration
+```
+or just `pytest` to run all tests.
