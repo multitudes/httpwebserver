475604cece33454c250b28b123706644a1767b48
Laurent Brusa <wrmultitudes@gmail.com>
2025-03-31 10:43:10 +0200

    small fix to keep it compiling

--- src/DirectoryListing.cpp
+++ src/DirectoryListing.cpp
@@ -41,37 +41,37 @@ namespace DirectoryListing
     //         return false;
     //     }
 
-        std::string dirString;
-        // Read directory contents
-        struct dirent *entry;
-        while ((entry = readdir(dir)) != NULL)
-        {
-            // Create an HTML list item for each entry
-            dirString += "<li><a href=\"";
-            dirString += connection.data.target;
-            dirString += entry->d_name;
-            dirString += "\">";
-            dirString += entry->d_name;
-            dirString += "</a></li>\n";
-        }
-        closedir(dir);
-        std::string htmlCode;
-        // add html code to the directory string
-        htmlCode = "<html><head><title>Directory Listing</title>";
-        htmlCode += "<style>";
-        htmlCode += "body {background-color: black; color: white;} a {color: lightblue;}";
-        htmlCode += "</style>";
-        htmlCode += "</head><body><h1>Index " + fullPath + "</h1><ul>" + dirString + "</ul></body></html>";
-       // htmlCode += "</head><body><h1>" + connection.data.target + "</h1><ul>" + dirString + "</ul></body></html>";
-        debuglog(GREEN, "Directory contents: \n%s", dirString.c_str());
-        // update connection state and data
-        connection.state = CONN_SIMPLE_RESPONSE;
-        // generate HTTP header and include html payload
-        SimpleResponse::addHTTPHeader(connection, TEXT_HTML, htmlCode, 200);
+    //     std::string dirString;
+    //     // Read directory contents
+    //     struct dirent *entry;
+    //     while ((entry = readdir(dir)) != NULL)
+    //     {
+    //         // Create an HTML list item for each entry
+    //         dirString += "<li><a href=\"";
+    //         dirString += connection.data.target;
+    //         dirString += entry->d_name;
+    //         dirString += "\">";
+    //         dirString += entry->d_name;
+    //         dirString += "</a></li>\n";
+    //     }
+    //     closedir(dir);
+    //     std::string htmlCode;
+    //     // add html code to the directory string
+    //     htmlCode = "<html><head><title>Directory Listing</title>";
+    //     htmlCode += "<style>";
+    //     htmlCode += "body {background-color: black; color: white;} a {color: lightblue;}";
+    //     htmlCode += "</style>";
+    //     htmlCode += "</head><body><h1>Index " + fullPath + "</h1><ul>" + dirString + "</ul></body></html>";
+    //    // htmlCode += "</head><body><h1>" + connection.data.target + "</h1><ul>" + dirString + "</ul></body></html>";
+    //     debuglog(GREEN, "Directory contents: \n%s", dirString.c_str());
+    //     // update connection state and data
+    //     connection.state = CONN_SIMPLE_RESPONSE;
+    //     // generate HTTP header and include html payload
+    //     SimpleResponse::addHTTPHeader(connection, TEXT_HTML, htmlCode, 200);
 
-    //     debuglog(BLUE, "Directory simple response: \n%s", connection.data.response.c_str());
+    // //     debuglog(BLUE, "Directory simple response: \n%s", connection.data.response.c_str());
 
-    //     return true;
+    // //     return true;
     }
 
 } // namespace DirectoryListing
