08533acb167c89981b33e468d69f77c36c4db420
emily-cloud <emilyaiai86@gmail.com>
2025-04-10 10:32:41 +0200

    add cookie variable

--- src/ServerData.hpp
+++ src/ServerData.hpp
@@ -25,6 +25,7 @@ struct Location {
 	bool autoindex;
 	bool file_upload;
 	bool internal;
+  bool cookie;
   std::string root;
   std::vector<std::string> acceptedMethods;
   std::pair<int, std::string> return_directive;
@@ -35,6 +36,7 @@ struct Location {
     autoindex(false),  // 4 same priority because different
     file_upload(false), // 4 same priority because different
     internal(false),
+    cookie(false),
 	  acceptedMethods(),  // 2 if post then could be upload - if not could be autoindex
     return_directive(),  // 1st - return immediately 
     root(""), // 5 check for new root yes no
