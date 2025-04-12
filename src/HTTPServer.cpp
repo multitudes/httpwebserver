e7e0472daf0b1cff152230b1ea851173b444e120
Laurent Brusa <wrmultitudes@gmail.com>
2025-04-12 20:08:19 +0200

    ave progress

--- src/HTTPServer.cpp
+++ src/HTTPServer.cpp
@@ -299,12 +299,27 @@ int run(std::string configFile) {
         }
         continue;
 
+
+
+
+
+
+
       } else if (conn.state == CONN_CGI) {
         debug("CONN_CGI fd %d", conn.client_fd);
         debuglog(YELLOW, "Connection fd %d in state CGI", conn.client_fd);
-        // check if the cgi is ready to be sent
-        // if not set to CONN_CLOSING
-        // else send the cgi
+
+        // check if the child process is pollin and i have data in buffer from the 
+        // preparecgi function
+
+        if (current_fd == conn.cgiData.cgi_stdin &&
+            (pollfds[i].revents & POLLOUT)) {
+          debug("CGI stdin fd %d", conn.client_fd);
+          // Send data to CGI process
+          ssize_t bytes_written =
+              write(conn.cgiData.cgi_stdin, conn.cgiData.buffer.c_str(), 
+                    conn.cgiData.buffer.size());
+            }
         // Handle data from client and send to cgi
         if (current_fd == conn.client_fd && (pollfds[i].revents & POLLIN)) {
           char buffer[BUFFER_SIZE];
