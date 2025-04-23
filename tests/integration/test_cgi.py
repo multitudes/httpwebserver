be020def180c64c5e61bd4724f8b641e0218bbed
Laurent Brusa <wrmultitudes@gmail.com>
2025-04-23 14:54:08 +0200

    put the edless loop test in a different file

--- tests/integration/test_cgi.py
+++ tests/integration/test_cgi.py
@@ -214,10 +214,3 @@ def test_cgi_perl_hello(webserver_normal_config):
     assert 'Also they are made of metal, whereas man is made of skin.<br>---<br></div>' in response.text, "Missing quote fragment: Metal/Skin"
     assert 'you are a mile away from them and you have their shoes.<br>---<br></div>' in response.text, "Missing quote fragment: Shoes"
 
-def test_cgi_endlessloop(webserver_normal_config):
-    url = 'http://localhost:4244/cgi/endlessloop.py'  
-    try: 
-        response = requests.get(url, timeout=1)
-    except Exception as e:
-        pytest.fail(f"An unexpected error occurred: {e}")
-    assert response.status_code == 504, f"Unexpected status code: {response.status_code}"
